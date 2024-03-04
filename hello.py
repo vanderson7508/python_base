05efbe03cd36decac870eb3c852b6094d43bfd74
vanderson7508 <missionariovanderson@gmail.com>
2024-03-03 21:34:48 -0300

    Implementamos o hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
+"""
+Hello World Multi Linguas.
+
+Dependendo da LINGUA CONFIGURADA no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+tenha a variaVEL LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execucao:
+
+    python3 hello.py
+    ou
+    ./hello.py
+
+if __name__ == "__main__":
+"""
+__version__= "0.0.1"
+__author__ = "Vanderson"
+__license__ = "Unlicense"
 
 #Este programa imprime Hello World
-print('Hello world'.upper())
-print(55 + 7)
-print('testando')
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde!"
+
+
+print(msg)
